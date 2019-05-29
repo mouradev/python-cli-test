4bcb64922d738b55ae7a83caf5324d8afd25cb4e
lucas <lucasmoura765@gmail.com>
2019-05-28 23:34:30 -0300

    mvp todo

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ months = json.dumps(months_json)
 print('Hello World py') #the print command on python 3 requires that use as a function() and use with 'single cotation marks'. In python the '' is more common that ""
 
 # waiting for inputs
-# name = input('Qual o seu nome? ')
+name = input('Qual o seu nome? ')
 
-# print('Seja bem vindo ao mundo python', name)
+print('Seja bem vindo ao mundo python', name)
 
-# birth_str = input('me diga sua data de nascimento no formato DD/MM/AAAA: ')
-# birth = datetime.datetime.strptime(birth_str, '%d/%m/%Y')
+birth_str = input('me diga sua data de nascimento no formato DD/MM/AAAA: ')
+birth = datetime.datetime.strptime(birth_str, '%d/%m/%Y')
 
 # print(birth)
-print(months)
+# print(months)
 # print('O seu aniversário é em', json["birth.strftime('%m')"])
-print(months["12"])
+# print(months["12"])
 
 print('Obrigado. Hora do registro:', datetime.datetime.now().strftime('%d/%m/%Y %H:%M'))
