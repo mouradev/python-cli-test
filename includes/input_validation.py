eb8ea8558d6e90424d66c7e4bc2a89c594e2f9d5
lucas <lucasmoura765@gmail.com>
2019-06-01 01:12:23 -0300

    validation of the input date

--- includes/input_validation.py
+++ includes/input_validation.py
@@ -3,6 +3,23 @@ def validate_date(str_date):
     import re
     regex = re.compile('.{2}/.{2}/.{4}')
     if regex.match(str_date) is not None:
+
+        # fazer o split da data para garantir que dias terão mais que 0 e menos que 32
+        # meses mais que 0 e menos que 13
+        # ano mais de 1000
+        splited_date = str_date.split('/')
+
+        valid_date = True
+        if(not (int(splited_date[0]) > 0 and int(splited_date[0]) < 32)):
+            valid_date = False
+        if(not (int(splited_date[1]) > 0 and int(splited_date[1]) < 13)):
+            valid_date = False
+        if(not (int(splited_date[2]) > 1000)):
+            valid_date = False
+        
+        if(not valid_date):
+            raise ValueError('Invalid date. Check the numbers')
+
         return str_date
     else:
-        return '00/00/0000'
+        raise ValueError('Invalid format for date. Please type a date in format dd/mm/yyyy')
