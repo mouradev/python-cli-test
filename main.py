92f346ce94b4b1965f58885ea61d599cf6f922eb
lucas <lucasmoura765@gmail.com>
2019-06-01 01:49:59 -0300

    implements validation to input date

--- main.py
+++ main.py
@@ -16,10 +16,16 @@ name = input('Qual o seu nome? ')
 
 print('Seja bem vindo ao mundo python', name)
 
-birth_str = input('me diga sua data de nascimento no formato DD/MM/AAAA: ')
-print(input_validation.validate_date(birth_str))
+date_is_valid = False
+while not date_is_valid:
+    birth_str = input('me diga sua data de nascimento no formato DD/MM/AAAA: ')
+    try:
+        birth_str = input_validation.validate_date(birth_str)
+        date_is_valid = True
+    except ValueError as e:
+        print(e)
+
 birth = datetime.datetime.strptime(birth_str, '%d/%m/%Y')
 
 # print('O seu aniversário é em', json["birth.strftime('%m')"])
-
 print('Obrigado. Hora do registro:', datetime.datetime.now().strftime('%d/%m/%Y %H:%M'))
